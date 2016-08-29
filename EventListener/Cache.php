964bb4db89001b98795183d1247952afe1bdb3a1
gilles <gilles.bourgeat@gmail.com>
2016-08-29 12:28:15 +0200

    First version

---
+++ EventListener/Cache.php
@@ -0,0 +1,49 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+namespace TheliaCachePolyfill\EventListener;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Thelia\Core\Event\Cache\CacheEvent;
+use Thelia\Core\Event\TheliaEvents;
+
+class Cache extends BaseAction implements EventSubscriberInterface
+{
+    /** @var AdapterInterface */
+    protected $adapter;
+
+    /**
+     * CacheListener constructor.
+     * @param AdapterInterface $adapter
+     */
+    public function __construct(AdapterInterface $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function cacheClear(CacheEvent $event)
+    {
+        // clear cache on thelia.cache service
+        $this->adapter->clear();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            TheliaEvents::CACHE_CLEAR => array('cacheClear', 128)
+        );
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Thelia Cache Polyfill
+
+This Polyfill add the service `thelia.cache` and the smarty plugin `cache`
+This Polyfill is not necessary for Thelia > 2.3.
+
+## Installation
+
+### Composer
+
+Add it in your main thelia composer.json file
+
+```
+composer require thelia/thelia-cache-polyfill-module:~1.0.0
+```
+
+## Usage
+
+[Thelia documentation](http://doc.thelia.net/en/documentation/modules/caching.html)
